449eeebfaaa9eb2bd129c4174582553fdbc84905
Matt Lemmone <mattlemmone@att.net>
2017-11-01 23:23:09 -0700

    added comments to demo file

--- scripts/health_monitor.py
+++ scripts/health_monitor.py
@@ -8,13 +8,16 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
+# All scripts must inherit from BaseScript
 class Script(BaseScript):
+    # True by default, even without this next line.
     script_active = True
 
+    # 'run' is the entry point of the script
     @staticmethod
     def run():
         player = Player()
 
         while True:
-            logger.debug('Player hp: %d', player.parameters.current_hp)
+            logger.debug('Player data: %r', player)
             time.sleep(1)
